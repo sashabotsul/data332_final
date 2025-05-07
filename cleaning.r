5557f5c36e283c980d2dde997180f1d326776bce
retflipper <32006126+retflipper@users.noreply.github.com>
2025-05-07 15:30:36 -0500

    Update cleaning.r

--- cleaning.r
+++ cleaning.r
@@ -53,9 +53,6 @@ df_mlb <- df_mlb_1985_2024 %>%
     median_salary = median(salary)
   )
 
-df_mlb_teams <- df_mlb_1985_2024 %>%
-  group_by(teamID) %>%
-  summarise(count = n())
 
 NBA_1984_2018_url <- getURL('https://raw.githubusercontent.com/sashabotsul/data332_final/refs/heads/main/data/NBA_Salaries_1985to2018.csv')
 df_NBA_1984_2018 <- read.csv(text = NBA_1984_2018_url)
@@ -100,7 +97,15 @@ df_nba <- df_NBA_1984_2018 %>%
     median_salary = median(salary)
   )
 
-df_nba_teams <- df_NBA_1984_2018 %>%
-  group_by(teamID) %>%
-  summarise(count = n())
+df_mlb <- df_mlb %>% mutate(sport = "MLB")
+df_nba <- df_nba %>% mutate(sport = "NBA")
 
+# Combine both datasets
+df_combined <- bind_rows(df_mlb, df_nba)
+
+# Add a source column to each dataset
+df_mlb_with_teams <- df_mlb_with_teams %>% mutate(sport = "MLB")
+df_nba_with_teams <- df_nba_with_teams %>% mutate(sport = "NBA")
+
+# Combine both datasets
+df_combined_with_teams <- bind_rows(df_mlb_with_teams, df_nba_with_teams)
